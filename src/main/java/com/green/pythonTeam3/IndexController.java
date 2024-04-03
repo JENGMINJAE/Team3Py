caee98fb21098ffbf43908ba19a5d2a71bc2a771
JENGMINJAE <alswo990602@naver.com>
2024-04-03 16:14:20 +0900

    layout & properties

--- src/main/java/com/green/pythonTeam3/IndexController.java
+++ src/main/java/com/green/pythonTeam3/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String mainPage(){
-        return "/main";
+        return "/main/main";
     }
 }

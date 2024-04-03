b76f1e19198b26c68a4595afe75440ebd7868e37
JENGMINJAE <alswo990602@naver.com>
2024-04-03 12:25:05 +0900

    first commit

---
+++ src/main/java/com/green/pythonTeam3/IndexController.java
@@ -0,0 +1,12 @@
+package com.green.pythonTeam3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String mainPage(){
+        return "/main";
+    }
+}

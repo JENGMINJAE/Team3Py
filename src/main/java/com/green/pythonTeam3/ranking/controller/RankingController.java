ba95a7dae2da0a5023083a535fd9585a08e581e5
thdals12345 <thdals12345@naver.com>
2024-05-02 10:39:39 +0900

    헤더 ê경로 수정

--- src/main/java/com/green/pythonTeam3/ranking/controller/RankingController.java
+++ src/main/java/com/green/pythonTeam3/ranking/controller/RankingController.java
@@ -1,10 +1,22 @@
 package com.green.pythonTeam3.ranking.controller;
 
+import com.green.pythonTeam3.ranking.sevice.RankingServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ranking")
 public class RankingController {
+
+    @Resource(name = "rankingService")
+    private RankingServiceImpl rankingService;
+
+    //화재피해장소 페이지
+    @GetMapping("/place")
+    public String place(){
+        return "/content/place/place";
+    };
 
 }
--- src/main/java/com/green/pythonTeam3/ranking/sevice/RankingServiceImpl.java
+++ src/main/java/com/green/pythonTeam3/ranking/sevice/RankingServiceImpl.java
@@ -1,4 +1,14 @@
 package com.green.pythonTeam3.ranking.sevice;
 
-public class RankingServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("rankingService")
+public class RankingServiceImpl implements RankingService{
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+
+
 }

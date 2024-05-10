5dc367bfeb0cbf208061c77ab842a6d9815d3fa3
JENGMINJAE <alswo990602@naver.com>
2024-05-10 15:07:58 +0900

    pihyetonggyeo

--- src/main/java/com/green/pythonTeam3/dmg/controller/DmgController.java
+++ src/main/java/com/green/pythonTeam3/dmg/controller/DmgController.java
@@ -1,10 +1,18 @@
 package com.green.pythonTeam3.dmg.controller;
 
 import com.green.pythonTeam3.dmg.service.DmgServiceImpl;
+import com.green.pythonTeam3.dmg.vo.FiveVO;
+import com.green.pythonTeam3.dmg.vo.SixVO;
 import jakarta.annotation.Resource;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/dmg")
@@ -13,7 +21,21 @@ public class DmgController {
     private DmgServiceImpl dmgService;
 
     @GetMapping("/dmgList")
-    public String dmgList(){
+    public String dmgList(Model model){
+        model.addAttribute("totalIjd",dmgService.totalIjd());
+        model.addAttribute("totalMoney",dmgService.totalMoney());
+        model.addAttribute("yearIjd",dmgService.ijdPp());
+        model.addAttribute("yearMoney",dmgService.moneyDmg());
         return "/content/dmg/dmg_list";
+    }
+    @ResponseBody
+    @PostMapping("/fetchInjPp")
+    public List<FiveVO> fetchInjPp(){
+        return dmgService.ijdPp();
+    }
+    @ResponseBody
+    @PostMapping("/fetchMoneyDmg")
+    public List<SixVO> fetchMoneyDmg(){
+        return dmgService.moneyDmg();
     }
 }
--- src/main/java/com/green/pythonTeam3/dmg/vo/DmgVO.java
+++
@@ -1,8 +0,0 @@
-package com.green.pythonTeam3.dmg.vo;
-
-import lombok.Data;
-
-@Data
-public class DmgVO {
-
-}
